6efd28f16a40f25da857d7dce5587985c6c66483
Patricia frey <132176260+PatriciaFrey@users.noreply.github.com>
2025-07-15 12:41:50 -0300

    Add files via upload

---
+++ listaportobello/src/main/java/lista/portobello/listaportobello/ListaportobelloApplication.java
@@ -0,0 +1,13 @@
+package lista.portobello.listaportobello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaportobelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaportobelloApplication.class, args);
+	}
+
+}
---
+++ listaportobello/src/test/java/lista/portobello/listaportobello/ListaportobelloApplicationTests.java
@@ -0,0 +1,13 @@
+package lista.portobello.listaportobello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaportobelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
